417ff97fe0f802f1091258e74b65a0ed44d742d1
Syarif09 <syarif.khalid@gmail.com>
2024-06-07 13:06:40 +0700

    first commit

---
+++ task1.js
@@ -0,0 +1,15 @@
+const cekHariKerja = (day) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const dataDay = ['senin', 'selasa', 'rabu', 'kamis', 'jumat']
+            let cek = dataDay.find((item) => {
+                return item === day
+            })
+            if (cek) {
+                resolve(cek)
+            } else {
+                reject(new Error('Hari ini bukan hari kerja'))
+            }
+        }, 3000)
+    })
+}
