4bda737cb063f4934cd04939c19c75b0fe898148
Syarif09 <syarif.khalid@gmail.com>
2024-06-10 14:29:55 +0700

    revisi

--- task4.js
+++ task4.js
@@ -1,13 +1,25 @@
 // Mengambil data dari API
 
-const endPoint = 'https://jsonplaceholder.typicode.com/users'
+// const endPoint = 'https://jsonplaceholder.typicode.com/users'
 
-    async function getName(endPoint) {
-        const response = await fetch(endPoint);
-        let result=[]
-        let names = await response.json()
-        result=names.map((data)=>data.name)
-        console.log(result);
-}
+//     async function getName(endPoint) {
+//         const response = await fetch(endPoint);
+//         let result=[]
+//         let names = await response.json()
+//         result=names.map((data)=>data.name)
+//         console.log(result);
+// }
       
-getName(endPoint)
+// getName(endPoint)
+
+// cara lain
+const endPoint = fetch('https://jsonplaceholder.typicode.com/users')
+
+endPoint.then(res => {
+    res.json().then(data => {
+        const names = data.map(people => people.name)
+        for (let i = 0; i < names.length; i++){
+            console.log(names[i])
+        }
+    })
+})
